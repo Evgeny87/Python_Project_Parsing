915643250bbf185cd89a147bb7d889d5e3250c2b
evgeny <sun_nik_off@mail.ru>
2022-12-19 15:35:08 +0300

    Modified text_replace

--- text_replace.py
+++ text_replace.py
@@ -1,3 +1,6 @@
+import re
+
+
 def link_replace(link):
     link = link.strip()
     while "  " in link:
@@ -24,6 +27,26 @@ def link_replace_tel(link):
         link = link.replace("+7", "8")
     link = link.strip()
     return link
+
+
+def link_replase_tel_number(tel_str=""):
+    x = re.sub(r"[^0-9]", "", tel_str)
+    x = x.replace(" ", "")
+    return (x)
+
+
+def link_replase_tel_number2(tel_str=""):
+    b = ""
+    x = re.findall('[0-9]+', tel_str)
+    for i in range(len(x)):
+        b = b + str(x[i])
+    b = b.replace(" ", "")
+    return (b)
+
+
+def link_replase_tel_name(tel_name_str=""):
+    x = re.sub(r"[^a-zA-Zа-яА-Я0-9@. ]", "", tel_name_str)
+    return (x)
 
 
 if __name__ == "__main__":
