efc55fc78de91271673b7b333e16bbf0caf38805
Evgeny87 <sun_nik_off@mail.ru>
2023-02-11 00:09:06 +0300

    Update text_replace.py

--- text_replace.py
+++ text_replace.py
@@ -38,9 +38,9 @@ def link_replase_tel_number(tel_str=""):
 
 def link_replase_tel_number2(tel_str=""):
     tel_number2 = ""
-    x = re.findall('[0-9]+', tel_str)
-    for i in range(len(x)):
-        tel_number2 = tel_number2 + str(x[i])
+    tel = re.findall('[0-9]+', tel_str)
+    for i in range(len(tel)):
+        tel_number2 = tel_number2 + str(tel[i])
     tel_number2 = tel_number2.replace(" ", "")
     return (tel_number2)
 
