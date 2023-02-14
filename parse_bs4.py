6966f2dc3bf2440a72a4ce7dd64296fdf6be0fe1
evgeny <sun_nik_off@mail.ru>
2023-02-14 23:17:16 +0300

    refactoring parse_bs4.py

--- parse_bs4.py
+++ parse_bs4.py
@@ -44,14 +44,14 @@ def url_to_parse(url_site="", kol_zapros=0, STEP=0):
             print("Ошибка timeout, url:", url_site)
             logger.error("Timeout Error: {errt}".format(errt=errt))
             logger.error("Ошибка timeout, url: {url_site}".format(url_site=url_site))
-        except requests.exceptions.ReadTimeout as errrt:
-            print("ReadTimeout Error:", errrt)
-            logger.error("ReadTimeout Error: {errrt}, url: {url_site}".format(errrt=errrt,
-                                                                              url_site=url_site))
-        except requests.exceptions.ConnectTimeout as errct:
-            print("ConnectTimeout Error:", errct)
-            logger.error("ConnectTimeout Error: {errct}, url: {url_site}".format(errct=errct,
-                                                                                 url_site=url_site))
+        # except requests.exceptions.ReadTimeout as errrt:
+        #     print("ReadTimeout Error:", errrt)
+        #     logger.error("ReadTimeout Error: {errrt}, url: {url_site}".format(errrt=errrt,
+        #                                                                       url_site=url_site))
+        # except requests.exceptions.ConnectTimeout as errct:
+        #     print("ConnectTimeout Error:", errct)
+        #     logger.error("ConnectTimeout Error: {errct}, url: {url_site}".format(errct=errct,
+        #                                                                          url_site=url_site))
         except requests.exceptions.TooManyRedirects as errtmr:
             print("TooManyRedirects Error:", errtmr)
             logger.error("TooManyRedirects Error: {errtmr},"
