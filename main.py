3a36ec8e9afe517c5dbe3e948bd2ddf2fee15731
Evgeny87 <sun_nik_off@mail.ru>
2023-02-15 10:58:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def main():
     # logger.warning('Vse horosho, shag 1,'
     #                'zakonchilsia: {vremy_now}'.format(vremy_now=vremy.vremy_now(start)))
     vremy_now = vremy.vremy_now(start)
-    logger.warning('Vse horosho, shag 1,'
-                   'zakonchilsia: %vremy_now', vremy_now)   
+    logger.warning('Vse horosho, shag 1, zakonchilsia: %vremy_now', vremy_now)   
     city_new = dict_new.dict_new(2)
     len_region_href1 = len(region1["href1"])
     for i in range(len_region_href1):
